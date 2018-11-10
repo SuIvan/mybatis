ed5c3fd2da9c966b825d89f71ba35aefb3996ba5
ivan <SYF2017i>
2018-11-10 16:39:12 +0800

    Initial commit

---
+++ src/main/java/com/test/model/Test.java
@@ -0,0 +1,51 @@
+package com.test.model;
+
+/**
+ * @author : ivan
+ * @version : v1.00
+ * @CreationDate : 2018/11/9 13:57
+ * @Description :
+ * @update : 修改人，修改时间，修改内容
+ * @see ：[相关类/方法]
+ */
+public class Test {
+    /**
+     * id
+     */
+    private String id;
+
+    /**
+     * name
+     */
+    private String name;
+
+    /**
+     * age
+     */
+
+    private Integer age;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
